99490a5a627768a3c0c00eef324d8a9b08673dbc
Felix <mail@nicidienase.de>
2019-07-19 12:10:04 +0200

    save all menus

--- app/src/main/java/de/nicidienase/geniesser_app/data/MenuRepository.kt
+++ app/src/main/java/de/nicidienase/geniesser_app/data/MenuRepository.kt
@@ -2,7 +2,6 @@ package de.nicidienase.geniesser_app.data
 
 import androidx.lifecycle.LiveData
 import de.nicidienase.geniesser_app.api.MenuApi
-import de.nicidienase.geniesser_app.api.SpeiseplanWrapperDto
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -14,7 +13,6 @@ class MenuRepository(
     private val dishDao by lazy { database.getDishDao() }
     private val locationDao by lazy { database.getLocationDao() }
 
-    fun getDishes() = dishDao.getAll()
     fun getDishesForLocation(locationId: Int) = dishDao.getAllForLocation(locationId)
     fun getDishesForDay(day: Long): LiveData<List<Dish>> = dishDao.getDishesForDay(day)
     fun getDays(locationId: Int) = dishDao.getAvailableDatesForLocation(locationId)
@@ -22,8 +20,9 @@ class MenuRepository(
     fun update(locationId: Int) = GlobalScope.launch {
         locationDao.insert(api.getLocations().mapNotNull { Location.fromDto(it) })
 
-        val menu: SpeiseplanWrapperDto = api.getMenu(locationId)[0]
+        val menus = api.getMenu(locationId)
         val categories = api.getMenuCategories(locationId)
-        dishDao.insert(menu.speiseplanGerichtData.mapNotNull { Dish.fromGerichtDto(it, locationId, categories) })
+        val dishes: List<Dish> = menus.flatMap { it.speiseplanGerichtData.mapNotNull { Dish.fromGerichtDto(it, locationId, categories) } }
+        dishDao.insert(dishes)
     }
 }
