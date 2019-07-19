797c551200c8eec2d0c15838ab579765ea7b6af1
Felix <mail@nicidienase.de>
2019-07-19 11:05:27 +0200

    add appcenter

--- app/src/release/java/de/nicidienase/geniesser_app/GourmetApplication.kt
+++ app/src/release/java/de/nicidienase/geniesser_app/GourmetApplication.kt
@@ -1,5 +1,14 @@
 package de.nicidienase.geniesser_app
 
 import android.app.Application
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
-class GourmetApplication : Application()
+class GourmetApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCenter.start(this, BuildConfig.APPCENTER_ID, Analytics::class.java, Crashes::class.java)
+    }
+}
