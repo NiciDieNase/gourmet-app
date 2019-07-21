ed5b586e68f472c0dfd529f9ecae973cfecd4505
Felix <mail@nicidienase.de>
2019-07-21 21:24:18 +0200

    fix formatting

--- app/src/main/java/de/nicidienase/geniesser_app/overview/MealOverviewFragment.kt
+++ app/src/main/java/de/nicidienase/geniesser_app/overview/MealOverviewFragment.kt
@@ -2,8 +2,6 @@ package de.nicidienase.geniesser_app.overview
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
