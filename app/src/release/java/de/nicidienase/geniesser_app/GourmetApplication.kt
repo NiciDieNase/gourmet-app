90674e64db172faf78ef57421881088c00437233
Felix <mail@nicidienase.de>
2019-10-03 16:33:18 +0200

    enable appcenter updates

--- app/src/release/java/de/nicidienase/geniesser_app/GourmetApplication.kt
+++ app/src/release/java/de/nicidienase/geniesser_app/GourmetApplication.kt
@@ -4,11 +4,18 @@ import android.app.Application
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
 
 class GourmetApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        AppCenter.start(this, BuildConfig.APPCENTER_ID, Analytics::class.java, Crashes::class.java)
+        AppCenter.start(
+            this,
+            BuildConfig.APPCENTER_ID,
+            Analytics::class.java,
+            Crashes::class.java,
+            Distribute::class.java
+        )
     }
 }
