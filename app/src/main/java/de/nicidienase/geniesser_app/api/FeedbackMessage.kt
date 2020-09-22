574282720b5c1fb966dacdd8de024badd0ab962b
Felix <mail@nicidienase.de>
2020-09-22 03:28:39 +0200

    add example for feedback-message

--- app/src/main/java/de/nicidienase/geniesser_app/api/FeedbackMessage.kt
+++ app/src/main/java/de/nicidienase/geniesser_app/api/FeedbackMessage.kt
@@ -16,3 +16,60 @@ data class FeedbackMessageItem(
     val value: Any,
     @SerializedName("zeitstempel") val timestamp: String
 )
+
+/*
+https://genussapp-api-mt.konkaapps.de/kms-mt-webservices/webresources/entity.gerichtfeedbackmessage/createAll
+{
+    "gerichtFeedbackMessage": [
+        {
+            "feedbackID": 32,
+            "gerichtFeedbackID": 1,
+            "gerichtID": 307183,
+            "value": 6,
+            "zeitstempel": "2020-09-22T03:02:59"
+        },
+        {
+            "feedbackID": 32,
+            "gerichtFeedbackID": 2,
+            "gerichtID": 307183,
+            "value": 6,
+            "zeitstempel": "2020-09-22T03:02:59"
+        },
+        {
+            "feedbackID": 32,
+            "gerichtFeedbackID": 3,
+            "gerichtID": 307183,
+            "value": 5,
+            "zeitstempel": "2020-09-22T03:02:59"
+        },
+        {
+            "feedbackID": 32,
+            "gerichtFeedbackID": 4,
+            "gerichtID": 307183,
+            "value": 5,
+            "zeitstempel": "2020-09-22T03:02:59"
+        },
+        {
+            "feedbackID": 32,
+            "gerichtFeedbackID": 5,
+            "gerichtID": 307183,
+            "value": 6,
+            "zeitstempel": "2020-09-22T03:02:59"
+        },
+        {
+            "feedbackID": 32,
+            "gerichtFeedbackID": 7,
+            "gerichtID": 307183,
+            "value": "TOP",
+            "zeitstempel": "2020-09-22T03:02:59"
+        },
+        {
+            "feedbackID": 32,
+            "gerichtFeedbackID": 6,
+            "gerichtID": 307183,
+            "value": 4,
+            "zeitstempel": "2020-09-22T03:02:59"
+        }
+    ]
+}
+ */
