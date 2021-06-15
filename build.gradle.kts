fcbb504b17f0f1dc171e9a31226a2118d0fa7c60
Felix <mail@nicidienase.de>
2021-06-15 11:45:27 +0200

    migrate projekt build.gradle to kts

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    val kotlin_version by extra("1.5.10")
+    val nav_version by extra("2.4.0-alpha02")
+    repositories {
+        google()
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.0-beta03")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.1.0")
+        classpath("com.google.gms:google-services:4.3.8")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+apply(plugin = "org.jlleitschuh.gradle.ktlint-idea")
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+}
